3ecc04f8ad16432f1735c783aabab9ecb2c01adc
Daniel Molloy <danieltmolloy919@gmail.com>
2024-11-03 12:34:52 +1100

    Progress on inputs

--- src/app/calculator.tsx
+++ src/app/calculator.tsx
@@ -5,6 +5,9 @@ import { Input } from "~/components/ui/input";
 import { Label } from "~/components/ui/label";
 import { Slider } from "~/components/ui/slider";
 
+const MAX_HECS_DEBT = 174998;
+const MAX_SALARY = 180000;
+
 type InputWithSliderProps = {
   label: string;
   value: number;
@@ -20,6 +23,9 @@ function InputWithSlider({
   min,
   max,
 }: InputWithSliderProps) {
+  const formatNumber = (num: number) => num.toLocaleString("en-US");
+  const parseNumber = (str: string) => parseInt(str.replace(/,/g, ""), 10);
+
   return (
     <div>
       <Label>{label}</Label>
@@ -27,9 +33,14 @@ function InputWithSlider({
         <div className="flex w-48 items-center gap-1">
           <span className="text-sm">$</span>
           <Input
-            type="number"
-            value={value}
-            onChange={(e) => onChange(Number(e.target.value))}
+            type="text"
+            value={formatNumber(value)}
+            onChange={(e) => {
+              const parsedValue = parseNumber(e.target.value);
+              if (!isNaN(parsedValue)) {
+                onChange(parsedValue);
+              }
+            }}
             min={min}
             max={max}
           />
@@ -46,17 +57,32 @@ function InputWithSlider({
 }
 
 export default function Calculator() {
-  const [hecsDebt, setHecsDebt] = useState<number>(1000);
+  const [hecsDebt, setHecsDebt] = useState<number>(30000);
+
+  const [salary, setSalary] = useState<number>(60000);
 
   return (
     <div>
-      <InputWithSlider
-        label="How big is your HECS debt?"
-        value={hecsDebt}
-        onChange={setHecsDebt}
-        min={0}
-        max={100000}
-      />
+      <div className="flex flex-col gap-4">
+        <InputWithSlider
+          label="How big is your HECS debt?"
+          value={hecsDebt}
+          onChange={setHecsDebt}
+          min={0}
+          max={MAX_HECS_DEBT}
+        />
+        <InputWithSlider
+          label="How much do you earn a year?"
+          value={salary}
+          onChange={setSalary}
+          min={0}
+          max={MAX_SALARY}
+        />
+      </div>
+      <div className="pt-4"></div>
+      <div>
+        <h2 className="text-2xl">Labour&apos;s changes</h2>
+      </div>
     </div>
   );
 }
